67c7a16ebc45c9c6f373735d1ba407b26d40a2b1
Paul McCarthy <paul.mccarthy@avant.com>
2017-11-15 20:05:16 -0600

    typo in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/name_check/" => "examples#name_logic"
+  get "/name_check" => "examples#name_logic"
   get "/number_game" => "examples#guess_a_number"
   get "number_game/:guess" => "examples#guess_a_number"
 end
